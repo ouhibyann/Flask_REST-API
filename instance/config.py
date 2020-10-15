34cb17d311c85a7222255a320aa18114dead7478
yann ouhib <yannis.ouhib@etu.u-pec.fr>
2020-10-15 17:58:52 +0200

    Reworked the app for better optimisation for testing, development and production

---
+++ instance/config.py
@@ -0,0 +1,34 @@
+import os
+
+
+class Config(object):
+    """Parent configuration class."""
+    DEBUG = False
+    CSRF_ENABLED = True
+    SECRET = os.getenv('SECRET')
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:admin@localhost/Metron"
+
+
+class DevelopmentConfig(Config):
+    """Configurations for Development."""
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    """Configurations for Testing, with a separate test database."""
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:admin@localhost/Metron'  # Should be different database but will do for the current use
+    DEBUG = True
+
+
+class ProductionConfig(Config):
+    """Configurations for Production."""
+    DEBUG = False
+    TESTING = False
+
+
+app_config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig,
+}
